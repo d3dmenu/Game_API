a1a87485463b88d475c7b4b84538f5ac6e7132c4
Game API <Tinnapat.r@ku.th>
2021-04-08 22:47:27 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import uvicorn 
-import numpy as np
-import warnings
-
 from fastapi import FastAPI, Form
 
 app = FastAPI()
-warnings.simplefilter("ignore")
 
 @app.get("/")
 async def main():
@@ -14,6 +10,3 @@ async def main():
 @app.post("/visual/")
 async def login(data: str = Form(...)):
     return {"data": usernadatame}
-
-if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8080, debug=True)
