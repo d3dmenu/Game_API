115adfbf477058ba86bc9961407925140f7cf0c4
Game API <Tinnapat.r@ku.th>
2021-04-08 23:01:34 +0700

    Initial commit

--- main.py
+++ main.py
@@ -10,3 +10,6 @@ async def main():
 @app.post("/visual/")
 async def login(data: str = Form(...)):
     return {"data": usernadatame}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host = "127.0.0.1", port = 8080)
